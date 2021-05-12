1ffd6eab6aa1beec430441b40c9df4ba4c1140c1
Sumitesh Naithani <sumiteshn@gmail.com>
2021-05-12 12:01:59 +0530

    Added CSES1079 & CSES1715

---
+++ Maths Algorithms/CSES1079.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.io.*;
+ 
+public class CSES1079 {
+ 
+	static BufferedReader br;
+	static PrintWriter pw;
+	static StringTokenizer st;
+ 
+	static int ni() {
+		return Integer.parseInt(st.nextToken());
+	}
+ 
+    /*
+		We can precalculate factorials and use fast modular exponentiation.
+		Inverse modulos can be calculated using Fermat's little theorem
+		because we have modulo under prime (1000_000_007)
+    */
+ 
+    static long mpow(long a , long b , long mod) {
+    	long ans = 1l;
+    	
+    	while (b > 0) {
+    		if (b % 2 == 1l) {
+    			ans *= a;
+    			ans %= mod;
+    		}
+    		b /= 2;
+    		a = a * a;
+    		a %= mod;
+    	}
+ 
+    	return ans;
+    }
+ 
+    static long modInv(long a , long mod) {
+    	return mpow(a , mod - 2l , mod);
+    }
+ 
+ 
+	public static void main(String args[]) throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		pw = new PrintWriter(System.out);
+		st = new StringTokenizer(br.readLine());
+ 		
+		int t = ni();
+		int maxN = 1000_002;
+		long mod = 1000_000_007;
+ 
+		long f[] = new long[maxN];
+		f[0] = 1l;
+ 
+		for (int i = 1; i < maxN; ++i) {
+			f[i] = (long)i * f[i - 1];
+			f[i] %= mod;
+		}
+ 
+		while (t --> 0) {
+			st = new StringTokenizer(br.readLine());
+			int a = ni();
+			int b = ni();
+			long ans = f[a];
+ 
+			long d1 = modInv(f[b] , mod);
+			long d2 = modInv(f[(a - b)] , mod);
+ 
+			ans *= d1;
+			ans %= mod;
+ 
+			ans *= d2;
+			ans %= mod;
+ 
+			pw.println(ans);
+		}
+ 
+		pw.close();
+		br.close();
+	}   
+}
---
+++ Maths Algorithms/CSES1715.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+ 
+public class CSES1715 {
+ 
+	static BufferedReader br;
+	static PrintWriter pw;
+
+    /*
+		Answer to the problem is just number of unique permutations
+		which is  n ! / (a ! * b ! * c ! ....) where a , b , c , d ... is
+		frequency of each character
+    */
+
+    static long mpow(long a , long b , long mod) {
+    	long ans = 1l;
+    	
+    	while (b > 0) {
+    		if (b % 2 == 1l) {
+    			ans *= a;
+    			ans %= mod;
+    		}
+    		b /= 2;
+    		a = a * a;
+    		a %= mod;
+    	}
+
+    	return ans;
+    }
+
+    static long modInv(long a , long mod) {
+    	return mpow(a , mod - 2l , mod);
+    }
+ 
+ 
+	public static void main(String args[]) throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		pw = new PrintWriter(System.out);
+ 		
+		String s = br.readLine();
+		int n = s.length();
+		long mod = 1000_000_007;
+
+		long f[] = new long[n + 1];
+		int cnt[] = new int[26];
+		f[0] = 1l;
+
+		for (int i = 1; i <= n; ++i) {
+			f[i] = (long)i * f[i - 1];
+			f[i] %= mod;
+			++cnt[s.charAt(i - 1) - 'a'];
+		}
+
+		long ans = f[n];
+
+		for (int i = 0; i < 26; ++i) {
+			ans *= modInv(f[cnt[i]] , mod);
+			ans %= mod;
+		}
+
+		pw.println(ans);
+
+		pw.close();
+		br.close();
+	}   
+}
