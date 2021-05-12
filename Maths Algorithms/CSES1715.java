c6be50e1ceedf2e5c0565474fd33704e4b0270be
Sumitesh Naithani <sumiteshn@gmail.com>
2021-05-12 12:06:50 +0530

    Indentation fix

--- Maths Algorithms/CSES1715.java
+++ Maths Algorithms/CSES1715.java
@@ -1,8 +1,8 @@
 import java.util.*;
 import java.io.*;
- 
+
 public class CSES1715 {
- 
+	
 	static BufferedReader br;
 	static PrintWriter pw;
 
@@ -12,55 +12,55 @@ public class CSES1715 {
 		frequency of each character
     */
 
-    static long mpow(long a , long b , long mod) {
-    	long ans = 1l;
-    	
-    	while (b > 0) {
-    		if (b % 2 == 1l) {
-    			ans *= a;
-    			ans %= mod;
-    		}
-    		b /= 2;
-    		a = a * a;
-    		a %= mod;
-    	}
+		static long mpow(long a , long b , long mod) {
+			long ans = 1l;
+			
+			while (b > 0) {
+				if (b % 2 == 1l) {
+					ans *= a;
+					ans %= mod;
+				}
+				b /= 2;
+				a = a * a;
+				a %= mod;
+			}
 
-    	return ans;
-    }
-
-    static long modInv(long a , long mod) {
-    	return mpow(a , mod - 2l , mod);
-    }
- 
- 
-	public static void main(String args[]) throws Exception {
-		br = new BufferedReader(new InputStreamReader(System.in));
-		pw = new PrintWriter(System.out);
- 		
-		String s = br.readLine();
-		int n = s.length();
-		long mod = 1000_000_007;
-
-		long f[] = new long[n + 1];
-		int cnt[] = new int[26];
-		f[0] = 1l;
-
-		for (int i = 1; i <= n; ++i) {
-			f[i] = (long)i * f[i - 1];
-			f[i] %= mod;
-			++cnt[s.charAt(i - 1) - 'a'];
+			return ans;
 		}
 
-		long ans = f[n];
-
-		for (int i = 0; i < 26; ++i) {
-			ans *= modInv(f[cnt[i]] , mod);
-			ans %= mod;
+		static long modInv(long a , long mod) {
+			return mpow(a , mod - 2l , mod);
 		}
+		
+		
+		public static void main(String args[]) throws Exception {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			pw = new PrintWriter(System.out);
+			
+			String s = br.readLine();
+			int n = s.length();
+			long mod = 1000_000_007;
 
-		pw.println(ans);
+			long f[] = new long[n + 1];
+			int cnt[] = new int[26];
+			f[0] = 1l;
 
-		pw.close();
-		br.close();
-	}   
-}
+			for (int i = 1; i <= n; ++i) {
+				f[i] = (long)i * f[i - 1];
+				f[i] %= mod;
+				++cnt[s.charAt(i - 1) - 'a'];
+			}
+
+			long ans = f[n];
+
+			for (int i = 0; i < 26; ++i) {
+				ans *= modInv(f[cnt[i]] , mod);
+				ans %= mod;
+			}
+
+			pw.println(ans);
+
+			pw.close();
+			br.close();
+		}   
+	}
